f27f1577303a9cb2fc58d10884638e5684f400de
Steven K <25186027+SteveKlutz@users.noreply.github.com>
2022-12-11 18:06:57 -0500

    Add files via upload

--- Airgead Banking App/main.cpp
+++ Airgead Banking App/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include "Savings.h"
 
+using namespace std;
+
+int main() {
+
+    // Previously tried "Savings::UserInputandValidation();" but received an error
+    // Instead, used instance to call function from class
+    Savings u;
+    u.UserInputandValidation();
+    Savings m;
+    double initialInvestmentAmount = 0;
+    double monthlyDeposit = 0;
+    double annualInterest = 0;
+    int years = 1;
+    m.myCurrentSavings(initialInvestmentAmount, monthlyDeposit, annualInterest, years);
+
+    return 0;
+}
