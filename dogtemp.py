577a420ccb84d1b32cfddea9e14b25b13aefbe41
Steven K <25186027+SteveKlutz@users.noreply.github.com>
2023-04-16 12:43:01 -0400

    Reorganized and updated entire code

--- dogtemp.py
+++ dogtemp.py
@@ -2,79 +2,83 @@
 # What will users want to know when opening app? ---> Date, Weather (Temp/Precip), your saved pets
 # What factors affect time dog can be outside? ---> Weather (Temp/Precip), dog size, dog fur type
 
-def dogtime():
+coat_op = ['short', 'long']
+size_op = ['small', 'medium', 'large']
+
+
+def intro():
     # opening statement
     print('How long can your dog be outside?')
     print('Answer the questions below to find out!')
     print('----------------------------------------------')
 
+
+def dog_size():
     # define input for dog size
-    dogsize = input('What size is your dog?\nSmall, Medium or Large?')
+    size = input('Enter the size of your dog.\nSmall, Medium or Large?\n')
+    while size not in size_op:
+        print('Incorrect entry. Try again.')
+        size = input()
+    print('Your dog is ' + size + ' sized.\n')
+    return size
 
-    if dogsize == 'Small':
-        print('Your dog is snack size.')
-    elif dogsize == 'Medium':
-        print('Your dog is medium size.')
-    elif dogsize == 'Large':
-        print('Your dog is a tank.')
-    else:
-        print('Incorrect entry.')
-        return
 
-    # define input for dog coat type
-    coattype = input('What is your dogs coat type?\nShort, Medium, Long, Double, Curly, Silky, Rough, Wire, Smooth or '
-                     'Hairless?\nNot sure? Enter "More info" and we can tell you what breeds are commonly associated '
-                     'each coat.')
+def dog_coat():
     # figure out how to implement coat type answers below
+    # define input for dog coat type
+    coat = input('What best describes your dogs coat type?\nShort or long?')
+    while coat not in coat_op:
+        # error message printed if anything but dog coat options is entered by user
+        print('Incorrect entry. Try again.\n')
+        coat = input()
+    else:
+        print('You selected a ' + coat + ' coat.\n')
+    return coat
 
+
+def dog_temp():
     # define input for outside temperature
-    temp = int(input('What is the temperature outside? (F)'))
+    temp = int(input('What is the temperature outside? (F)\n'))
+    return temp
 
-    # create answers for user depending on dog size, dog coat type, and outside temperature
-    if dogsize == 'Small' or dogsize == 'Medium':
-        if 20 < temp < 32:
+
+def results():
+    if (size == 'small' or 'medium' or 'large') and coat == 'short':
+        if 1 < temp < 19:
+            print('5 minutes or less.')
+        elif 20 < temp < 32:
             print('10 to 15 minutes.')
-        elif 70 < temp < 90:
-            print('A few hours will be fine.')
         elif 33 < temp < 59:
-            print('About an hour.')
+            print('About 30 minutes.')
+        elif 70 < temp < 90:
+            print('About an hour should be fine. Try to keep them out of direct sunlight.')
         elif 90 < temp < 99:
             print('5 minutes or less.')
-        elif 1 < temp < 19:
+        else:
+            print('Not a good idea to be outside til temperature changes.')
+    elif (size == 'small' or 'medium' or 'large') and coat == 'long':
+        if 1 < temp < 19:
             print('5 minutes or less.')
-        elif 100 < temp < 0:
-            print('Probably not a good idea to be outside til temperature changes.')
-        else:
-            print('Incorrect entry.')
-            return
-
-    elif dogsize == 'Large':
-        if temp < 32 or temp > 90:
+        elif 20 < temp < 32:
             print('10 to 15 minutes.')
-        elif 60 < temp < 90:
-            print('About an hour.')
         elif 33 < temp < 59:
-            print('A couple hours will be fine.')
+            print('About an hour.')
+        elif 70 < temp < 90:
+            print('About 30 minutes. Try to keep them out of direct sunlight.')
+        elif 90 < temp < 99:
+            print('In and out.')
         else:
-            print('Incorrect entry.')
-            return
+            print('Not a good idea to be outside til temperature changes.')
+    return results
 
-    # print coat examples if user enters "More info"
-    elif dogsize == 'More info':
-        print('Short: Pointers, Doberman, and Boxers\n'
-              'Medium: Germain Shepherds, Golden Retrievers, and Belgian Tervurens\n'
-              'Long: Afghan Hounds, Old English Sheepdogs, and Bearded \n'
-              'Double: Bernese Mountain Dog, Beagle, Newfoundland, Husky\n'
-              'Curly: Portuguese Water Dogs, Airedale Terriers and Poodles\n'
-              'Silky: Silky Terrier, Yorkshire Terrier, Afghan Hound and Irish Setter\n'
-              'Rough: Rough Collie\n'
-              'Wire: German Wirehaired Pointer, the Airedale Terrier, and the Wire Fox Terrier\n'
-              'Smooth: Bloodhounds, Dalmatians, Great Danes, and French Bulldogs\n'
-              'Hairless: Chinese Crested, the Xoloitzcuintli, and American Hairless Terrier')
 
-    # error message printed if anything but 3 dog size options is entered by user
-    else:
-        print('Incorrect entry.')
-        return
+intro()
+size = dog_size()
+coat = dog_coat()
+temp = dog_temp()
 
-dogtime()
+print('\n')
+print('Summary:\nYour dog is ' + str(size) + ' sized with a ' + str(coat) + ' coat and the current temperature is '
+      + str(temp) + '.\n')
+print('Results:')
+results()
